23e04810d77486d356fe3c29038a09c886daa9f0
Kyle Zhou <kylezhou2002@hotmail.com>
2020-11-16 19:16:31 -0800

    Initial Commit

---
+++ SMSBackupViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMSBackupViewer
+//
+//  Created by Kyle Zhou on 2020-11-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
